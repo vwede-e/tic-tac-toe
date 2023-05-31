0ca69b565a3fdb03712cd1c4f593a9d9604ed849
vwede-e <115898669+vwede-e@users.noreply.github.com>
2023-05-31 14:55:58 +0100

    Working tic tac toe game

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const gridContainer = document.querySelector(".grid-container");
+const modalClose = document.querySelector("aside > div.close");
+const announce = document.querySelector("aside > div.announce");
+const player1 = "X";
+const player2 = "O";
+let lastPlayed = player1;
+let gameBoardCounter = 0;
 window.addEventListener("load", createGrid);
 function createGrid() {
     for (let i = 0; i <= 8; i++ ) {
@@ -14,7 +20,69 @@ gridContainer.addEventListener("click", upDate);
 function upDate(event) {
     if (event.target != this) {
         const dataValue = +(event.target.getAttribute("data-value"));
-        event.target.textContent = "X";
-        gameBoard[dataValue-1] = "X";
+        event.target.textContent = lastPlayed;
+        gameBoard[dataValue-1] = lastPlayed;
+        gameBoardCounter += 1;
+        checkWin(dataValue);
     }
+}
+
+const winningCombination = [[1,2,3], [1,4,7], [1,5,9], [2,5,8], [3,6,9], [4,5,6], [3,5,7], [7,8,9]]
+
+function checkWin(dataValue) {
+    const arr = winningCombination.filter((x)=> x.includes(dataValue));
+    for (const i of arr) {
+        let result = i.every((x)=> gameBoard[x-1] === lastPlayed);
+        if (result) {
+            setTimeout(announceWin, 200);
+            return;
+        }
+    };
+    if (gameBoardCounter >=9) {
+        setTimeout(announceTie, 200);
+    }
+    else {
+        switchTurns();
+    };
+    };
+
+function switchTurns() {
+    lastPlayed = lastPlayed === player1 ? player2 : player1;
+}
+
+function emptyGrid() {
+    gridChildren = gridContainer.children;
+    gridChildrens = Array.from(gridChildren);
+    gridChildrens.forEach((x)=> x.textContent = "");
+}
+
+function announceWin() {
+   gridContainer.style.display = "none";
+   const aside = document.querySelector("aside");
+   aside.style.display = "flex";
+   announce.textContent = `Player ${lastPlayed} won!`;
+   modalClose.addEventListener("click", restart);
+}
+
+function announceTie() {
+    gridContainer.style.display = "none";
+    const aside = document.querySelector("aside");
+    aside.style.display = "flex";
+    announce.textContent = `A tie!`;
+    modalClose.addEventListener("click", restart);
+}
+
+function restart() {
+    // rewrite game board
+    for (let i = 0; i < gameBoard.length; i++) {
+        gameBoard[i] = i+i;
+    //empty html cells
+    emptyGrid();
+    const aside = document.querySelector("aside");
+    aside.style.display = "none";
+    gridContainer.style.display = "grid";
+    lastPlayed = player1;
+    gameBoardCounter = 0;
+    //
+}
 }
